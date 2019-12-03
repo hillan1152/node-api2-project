2b776c7f8e891e57a932c653a862d248531edd2f
Anthony Hill <hillan1152@gmail.com>
2019-12-03 18:09:53 -0500

    get request for specific id

--- express/lordRouter.js
+++ express/lordRouter.js
@@ -23,6 +23,22 @@ router.get('/', (req, res) => {
         })
 })
 
+// GET Request for specific posts
+router.get('/:id', (req, res) => {
+    db.findById(req.params.id)
+        .then(post => {
+            console.log(post)
+            if(post[0]){
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({ error: "The posts information could not be retrieved." })
+        })
+})
+
 // POST Request for adding info inside of the request body
 router.post('/', (req, res) => {
     console.log(req.body)
@@ -38,9 +54,25 @@ router.post('/', (req, res) => {
             res.status(500).json({ error: "There was an error while saving the post to the database" })
         })
     }
-
 })
-
+// POST FOR COMMENTS AND :ID REQUESTS
+// NEEDS WORK
+router.post('/:id/comments', (req, res) => {
+    if(!req.params.id){
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+    } else if (!req.body.text) {
+        res.status(400).json({ errorMessage: "Please provide text for the comment." })
+    } else {
+        db.insertComment(req.body)
+        .then(comment => {
+            res.status(201).json({...req.body, comment})
+        })
+        .catch(err => {
+            console.log('Post Request Error', err)
+            res.status(500).json({ error: "There was an error while saving the post to the database" })
+        })
+    }
+})
 
 
 // export router
